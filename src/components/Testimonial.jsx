addf9c96adda5128222e0fc244796938de4a364a
Pravin Boopathi <pravinboopathi1100@gmail.com>
2024-07-08 13:05:23 +0530

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -13,7 +13,7 @@ const Testimonial = () => {
   return (
     <div id='testimonial' className="testimonials-container bg-white text-blue-700 py-12">
   
-      <div class="elfsight-app-90157461-fe6c-4866-82d3-687964784247" data-elfsight-app-lazy></div>
+      <div class="elfsight-app-8a12bbaf-6508-47d2-b8e0-9db0ad87cec5" data-elfsight-app-lazy></div>
     </div>
   );
 };
